e3b4632688c076d0e9b350761f9129bf4f7606b9
Federico <fedmich@gmail.com>
2024-05-26 19:16:47 +0800

    Update Create-Repo-Automation.sh

--- Create-Repo-Automation.sh
+++ Create-Repo-Automation.sh
@@ -1,3 +1,9 @@
 echo [github-token] | gh auth login --with-token
 
 gh repo create [repo] --public
+
+
+```
+git config --global credential.helper store
+git config --global credential.helper "cache --timeout 43200"
+```
